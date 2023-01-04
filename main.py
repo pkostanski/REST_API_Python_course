f88c6b1cd1bd8e761bc2b585a5fa639272e7c3c4
pkostanski <p.m.kostanski@gmail.com>
2023-01-04 12:36:40 +0100

    Created endpoint returning users without passwords

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
+from pymongo import MongoClient
 
 
 app = FastAPI()
 
+
+class User(BaseModel):
+    FirstName: str
+    LastName: str
+    email: str
+    password: str # on production ofc hashed
+
+
 @app.get("/", tags=["category1"])
 def read_root():
-    return {"Hello": "from root1"}
-
-@app.get("/root2", tags=["category2"])
-def read_root2():
     """This endpoint is for test only"""
-    return {"Hello": "from root2"}
+    return {"Hello": "from root"}
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item name": item.name, "item id": item.id}
